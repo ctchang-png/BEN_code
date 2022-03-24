0bc0c7d1a430ae3ea1c9b9f31813682030f94da9
ctchang <ctchang001@gmail.com>
2022-03-23 22:38:51 -0400

    Update LED_test.py

--- LED_test.py
+++ LED_test.py
@@ -1,6 +1,7 @@
 import board
 import neopixel
 import numpy as np
+import time
 
 
 
@@ -16,6 +17,8 @@ pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.1, auto_write=Fals
 def main():
     for i in range(pixel_num):
         pixels[i] = 0
+        pixels.show()
+        time.sleep(0.1)
         
 
 if __name__ == "__main__":
