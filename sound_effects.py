86bab8ee893fca04704c9bec8d04d8483c3c0096
Eran Ozsarfati <eranozsarfati@gmail.com>
2022-03-28 19:37:05 -0400

    fixed sound effects

--- sound_effects.py
+++ sound_effects.py
@@ -1,33 +1,47 @@
 import time
 
-SOUND_EFFECTS = dict() 
-#key: "str"
-#val: TBD, could be a str path to sounds folder, could be mp4, could be others...
+from playsound import playsound
+from pynput.keyboard import Key, Listener
 
-def play_sound(SOUND_EFFECTS_VAL):
-    raise NotImplementedError("Write me!")
+# Library that maps effect names to relative paths
+SOUND_EFFECTS = {"smp": "sample.wav"}
 
-def do_sound_effect(sound_effect):
-    #Consult Chris before changing this fucntion
-    if sound_effect not in SOUND_EFFECTS:
-        print("Sound effect with keyword \"{}\" not in library".format(sound_effect))
-        return
-    play_sound(SOUND_EFFECTS[sound_effect])
+
+def play_sound(sound_effect_pth):
+    """
+    Given a path for an audio file, plays it
+
+    :param sound_effect_pth: path of audio file
+    """
+    playsound(sound_effect_pth)
+
+
+def do_sound_effect(sound_effect_key):
+    """
+    Plays a sound effect if it's in the library
+
+    :param sound_effect_key: key of audio file
+    """
+    if sound_effect_key not in SOUND_EFFECTS:
+        print(f"Sound effect with keyword '{sound_effect_key}' not in library")
+    else:
+        play_sound(SOUND_EFFECTS[sound_effect_key])
 
 
 if __name__ == "__main__":
     ### Keypress driver for testing
-    from pynput.keyboard import Key, Listener
+
     keys = set()
-    VALID_KEYS = ["1"]                                  #FILL ME
-    KEYBOARD_2_EFFECT = {"1": "test_audio"}             #FILL ME
+    KEYBOARD_EFFECT = {"1": "smp"}  # FILL ME
+
     def on_press(key):
         try:
             k = key.char
         except:
             k = key.name
-        if k in VALID_KEYS:
+        if k in KEYBOARD_EFFECT.keys():
             keys.add(k)
+
     def on_release(key):
         if key == Key.esc:
             # Stop listener
@@ -36,18 +50,18 @@ if __name__ == "__main__":
             k = key.char
         except:
             k = key.name
-        if k in VALID_KEYS:
+        if k in KEYBOARD_EFFECT.keys():
             keys.remove(k)
-    listener = Listener(
-            on_press=on_press,
-            on_release=on_release) 
+
+    listener = Listener(on_press=on_press, on_release=on_release)
     listener.start()
 
     while True:
         if len(keys) > 1:
-            print("Warning! Multiple keys activated. To avoid indeterminant behavior depress only 1 key at a time")
-        if len(keys) == 0:
+            print(
+                "Warning! Multiple keys activated. To avoid indeterminant behavior depress only 1 key at a time"
+            )
+        elif len(keys) == 0:
             continue
-        sound_effect = KEYBOARD_2_EFFECT[keys[0]] #Take first active key if any
-        do_sound_effect(sound_effect)
+        do_sound_effect(KEYBOARD_EFFECT[list(keys)[0]])
         time.sleep(0.100)
