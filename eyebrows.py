1e017d2c9c980b7adaa8a1b853b48f34b1e4afb4
chang-pi <ctchang001@gmail.com>
2022-04-02 01:17:01 -0400

    servo range testing

--- eyebrows.py
+++ eyebrows.py
@@ -16,9 +16,11 @@ class Servo():
         self.pwm = pwm
         self.angle = 0
         self.set_angle(0)
+        time.sleep(0.50)
+        print("Servo object at PIN {} created".format(pin))
     
     def set_angle(self, angle):
-        angle = angle % 360 #Stupid dumbass servo behavior here
+        angle = angle % 180 #approximate 180 deg range
         if angle < self.angle_min:
             #print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
             #angle = self.angle_min
@@ -27,7 +29,10 @@ class Servo():
             #print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
             #angle = self.angle_max
             None
-        duty = angle * (100/360)
+        #duty = angle * (100/360)
+        #duty range (5, 34) * 100/360 = (1.5, 9.5)
+        duty = ((9.5-1.5)/(180))*angle + 1.5
+        print(duty)
         self.pwm.ChangeDutyCycle(duty)
         self.angle = angle
 
@@ -40,10 +45,10 @@ servo1 = Servo(3, angle_min=-30, angle_max=30)
 angle = 0
 while True:
     try:
-        angle += 5
+        angle += 10
         print(angle)
         servo1.set_angle(angle)
-        time.sleep(0.5)
+        time.sleep(1.0)
     except KeyboardInterrupt:
         servo1.shutdown()
         GPIO.cleanup()
