d3033b4acf854c78e1234b921f0b5275b5e81309
chang-pi <ctchang001@gmail.com>
2022-04-02 00:35:55 -0400

    servo tweaks

--- eyebrows.py
+++ eyebrows.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(gpio.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
 
 class Servo():
@@ -11,29 +11,34 @@ class Servo():
         self.angle_max = angle_max
         GPIO.setup(pin, GPIO.OUT)
         pwm = GPIO.PWM(pin, 50)
-        pwm.start(0
-        self.pwm = pwm)
+        pwm.start(0)
+        self.pwm = pwm
         self.angle = 0
         self.set_angle(0)
     
     def set_angle(self, angle):
+        angle = angle % 360 #Stupid dumbass servo behavior here
         if angle < self.angle_min:
-            angle = self.angle_min
-            print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
+            #print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
+            #angle = self.angle_min
+            None
         if angle > self.angle_max:
-            angle = self.angle_max
-            print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
-        duty = angle / 18 + 2
+            #print("Angle {:0.2f} exceeds angle limits of ({:0.2f},{:0.2f}) [degrees]".format(angle, self.angle_min, self.angle_max))
+            #angle = self.angle_max
+            None
+        duty = (angle / 18 + 2) % (360/18)
         GPIO.output(self.pin, True)
         self.pwm.ChangeDutyCycle(duty)
         time.sleep(1)
         GPIO.output(self.pin, False)
-        self.pwm.ChangeDutyCycle(0)     
+        self.pwm.ChangeDutyCycle(0)
+        self.angle = angle
 
 servo1 = Servo(3, angle_min=-30, angle_max=30)
 while True:
     #rotate by increments of 90 degrees
     angle = (servo1.angle + 90) % 360
+    print(angle)
     servo1.set_angle(angle)
     time.sleep(1.0)
 
