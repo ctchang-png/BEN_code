2789b869f11313132cd8ea0397f358e73fe3537f
Len Huang <lendevelops@gmail.com>
2022-04-05 22:37:20 -0400

    casting issue

--- motors.py
+++ motors.py
@@ -149,10 +149,10 @@ if __name__ == "__main__":
         angle2 = input("enter servo2 angle btwn {} and {}: ".format(-20, 20))
         angle3 = input("enter servo3 angle btwn {} and {}: ".format(-20, 20))
         angle4 = input("enter servo4 angle btwn {} and {}: ".format(-20, 20))
-        servo1.set_angle(int(-angle1))
+        servo1.set_angle(-int(angle1))
         servo2.set_angle(int(angle2))
         servo3.set_angle(int(angle3))
-        servo4.set_angle(int(-angle4))
+        servo4.set_angle(-int(angle4))
 
 '''
 Eyebrows In (Angry) Neutral
