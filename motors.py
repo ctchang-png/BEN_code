50b6049a2600ac0674ef629ef3c14a30c252a3e7
Len Huang <lendevelops@gmail.com>
2022-04-05 22:40:42 -0400

    more descriptive names

--- motors.py
+++ motors.py
@@ -145,14 +145,14 @@ if __name__ == "__main__":
         # servo4.set_angle(-int(angle))
 
         print("")
-        angle1 = input("enter servo1 angle btwn {} and {}: ".format(-20, 20))
-        angle2 = input("enter servo2 angle btwn {} and {}: ".format(-20, 20))
-        # angle3 = input("enter servo3 angle btwn {} and {}: ".format(-20, 20))
-        # angle4 = input("enter servo4 angle btwn {} and {}: ".format(-20, 20))
+        angle1 = input("leftHeight btwn {} and {}: ".format(-20, 20))
+        angle2 = input("leftAngle btwn {} and {}: ".format(-20, 20))
+        angle3 = input("rightHeight btwn {} and {}: ".format(-20, 20))
+        angle4 = input("rightAngle btwn {} and {}: ".format(-20, 20))
         servo1.set_angle(-int(angle1))
         servo2.set_angle(int(angle2))
-        servo3.set_angle(0)
-        servo4.set_angle(0)
+        servo3.set_angle(int(angle3))
+        servo4.set_angle(-int(angle4))
 
 '''
 Eyebrows In (Angry) Neutral
