29da2861510c65e2eb3a649c380107316317cfa6
ctchang <ctchang001@gmail.com>
2022-04-07 02:12:08 -0400

    Update motors.py

--- motors.py
+++ motors.py
@@ -50,9 +50,9 @@ class Servo():
 
 class Eyebrows():
     def __init__(self):
-        self.hl = Servo(2, angle_min=-10, angle_max=15, bias=0)
+        self.hl = Servo(2, angle_min=-10, angle_max=15, bias=10)
         self.al = Servo(3, angle_min=-15, angle_max=15, bias=0)
-        self.hr = Servo(4, angle_min=-10, angle_max=15, bias=15)
+        self.hr = Servo(4, angle_min=-10, angle_max=15, bias=10)
         self.ar = Servo(14, angle_min=-15, angle_max=15, bias=25)
         self.jaw = Servo(15, angle_min=-50, angle_max=0, bias=-30)
 
@@ -270,9 +270,9 @@ class Eyebrows():
 
 
 if __name__ == "__main__":
-    servo1 = Servo(3, angle_min=-15, angle_max=15, bias=0)
+    servo1 = Servo(3, angle_min=-15, angle_max=15, bias=10)
     servo2 = Servo(5, angle_min=-15, angle_max=15, bias=0)
-    servo3 = Servo(7, angle_min=-100, angle_max=100, bias=15)
+    servo3 = Servo(7, angle_min=-100, angle_max=100, bias=10)
     servo4 = Servo(8, angle_min=-100, angle_max=100, bias=25)
     servo5 = Servo(10, angle_min=-100, angle_max=100, bias=-30)
     while True:
