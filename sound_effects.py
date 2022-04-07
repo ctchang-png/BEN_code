772543d15e04f94b6e4054efa4e084afd652f3be
ctchang <ctchang001@gmail.com>
2022-04-06 23:29:04 -0400

    Update sound_effects.py

--- sound_effects.py
+++ sound_effects.py
@@ -1,32 +1,6 @@
-import time
-
-from playsound import playsound
-
-# Library that maps effect names to relative paths
-SOUND_EFFECTS = {"smp": "sample.wav"}
-
-''''
-def play_sound(sound_effect_pth):
-    """
-    Given a path for an audio file, plays it
-
-    :param sound_effect_pth: path of audio file
-    """
-    playsound(sound_effect_pth)
-
-
-def do_sound_effect(sound_effect_key):
-    """
-    Plays a sound effect if it's in the library
-
-    :param sound_effect_key: key of audio file
-    """
-    if sound_effect_key not in SOUND_EFFECTS:
-        print(f"Sound effect with keyword '{sound_effect_key}' not in library")
-    else:
-        play_sound(SOUND_EFFECTS[sound_effect_key])
-'''
-
-if __name__ == "__main__":
-        playsound("sample.wav")
-        time.sleep(0.100)
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("sample.wav")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
